b67d227b50b8c92eaf99d1681c69eb42058cb570
Dolo <dolomac99@gmail.com>
2020-09-22 23:11:37 +0200

    Create example-autograph.cs

---
+++ msp/example-autograph.cs
@@ -0,0 +1,25 @@
+static async Task Main() {
+
+	string username, password, actor;
+
+	username = "Username";
+	password = "Password";
+	actor = "Pixi Star";
+
+	var mspClient = new MspClient(Server.Germany);
+
+	var mspLogin = await mspClient.Login(username, password);
+
+	if (!mspLogin.LoggedIn) return;
+
+	var (Result, Error, Success) = await mspClient.GetActorIdByName(actor);
+
+	if (!Success) return;
+
+	var AResult = await mspClient.SendAutograph(Result.Value);
+
+	if (!AResult.Success) return;
+
+	Console.ReadLine();
+
+}
