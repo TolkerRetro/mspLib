8eeab2a8613dfd8c4920f7ee6e0c20a4fcf996b4
Dolo <dolomac99@gmail.com>
2020-09-25 15:23:42 +0200

    Update example-petspet.cs

--- msp/example-petspet.cs
+++ msp/example-petspet.cs
@@ -1,16 +1,5 @@
 static async Task Main() {
 
-	string username, password;
-
-	username = "Username";
-	password = "Password";
-
-	MspClient mspClient = new MspClient(Server.Germany);
-
-	Login mspLogin = await mspClient.Login(username, password);
-
-	if (!mspLogin.LoggedIn) return;
-
 	var (Result, Error, Success) = await mspClient.GetBonsterListByActor(mspLogin.Actor.ActorId);
 
 	if (!Success) return;
