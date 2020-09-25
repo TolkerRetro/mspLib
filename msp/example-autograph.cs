c12b6cb00fee525e6378eaaa30f04d31730a40d9
Dolo <dolomac99@gmail.com>
2020-09-25 15:22:36 +0200

    removed login

--- msp/example-autograph.cs
+++ msp/example-autograph.cs
@@ -1,18 +1,6 @@
 static async Task Main() {
 
-	string username, password, actor;
-
-	username    = "Username";
-	password    = "Password";
-	actor       = "Pixi Star";
-
-	MspClient mspClient = new MspClient(Server.Germany);
-
-	Login mspLogin = await mspClient.Login(username, password);
-
-	if (!mspLogin.LoggedIn) return;
-
-	var (Result, Error, Success) = await mspClient.GetActorIdByName(actor);
+	var (Result, Error, Success) = await mspClient.GetActorIdByName("Pixi Star");
 
 	if (!Success || Result.Value == 0) return;
 
