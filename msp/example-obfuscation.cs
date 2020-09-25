3552362800d8f9188ca1db8ce720e898aa6f85e7
Dolo <dolomac99@gmail.com>
2020-09-25 15:36:15 +0200

    Update example-obfuscation.cs

--- msp/example-obfuscation.cs
+++ msp/example-obfuscation.cs
@@ -1,19 +1,20 @@
 // load obfuscated picture from eg. looks, images, movies ..
 
 static async Task Main() {
-	var R = await mspClient.GetNewUploads(100);
+	
+	  var R = await mspClient.GetNewUploads(100);
 
-	if (!R.Success) return;
+	  if (!R.Success) return;
 
 	  List<(string Url, GetNewUploadsResult Result)> ImageData = new List<(string Url, GetNewUploadsResult Result)>();
 
 	  R.Result.ForEach(async a => { 
    
-  (Image Image, string ImageUrl) img = await ServiceCommands.LoadObfuscatedImage(a.ImageUploadId, Server.Germany, ObfuscationType.pictureupload);
+          (Image Image, string ImageUrl) img = await ServiceCommands.LoadObfuscatedImage(a.ImageUploadId, Server.Germany, ObfuscationType.pictureupload);
 
-		if (img.Image is null) return;
+          if (img.Image is null) return;
 
-		ImageData.Add((img.ImageUrl, a));
+	  ImageData.Add((img.ImageUrl, a));
     
 	});
 
