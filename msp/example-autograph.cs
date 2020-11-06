6d36f71387d2c665f01dba6c55d14d1b2bb765a0
Dolo <dolomac99@gmail.com>
2020-11-06 03:37:08 +0100

    new update

--- msp/example-autograph.cs
+++ msp/example-autograph.cs
@@ -2,7 +2,7 @@ static async Task Main() {
 
 	var (Result, Error, Success, Json) = await mspClient.GetActorIdByName("Pixi Star");
 
-	if (!Success || Result.Value == 0) return;
+	if (!Success && Result == 0) return;
 
 	SendAutograph AResult = await mspClient.SendAutograph(Result);
 
