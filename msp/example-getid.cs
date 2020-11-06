784d8ea17e58824fe1cb10ca84fc2d5a299dc3ef
Dolo <dolomac99@gmail.com>
2020-11-06 03:36:36 +0100

    new update

--- msp/example-getid.cs
+++ msp/example-getid.cs
@@ -2,7 +2,7 @@ static async Task Main() {
 
 	var (Result, Error, Success, Json) = await mspClient.GetActorIdByName("Pixi Star");
 
-	if (!Success || Result == 0) return;
+	if (!Success && Result == 0) return;
 
 	Console.WriteLine($ "ID: {Result} = Pixi Star");
 
